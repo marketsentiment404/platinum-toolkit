61ca1c94c91204f309ef1e7b51eba921cd66d0f8
PlatinumFinance <86154134+polydaedalus@users.noreply.github.com>
2021-07-03 14:33:28 +0300

    initial commit

---
+++ packages/pancake-uikit/src/theme/colors.ts
@@ -0,0 +1,76 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#DD5252",
+  primary: "#001F4C",
+  primaryBright: "#001F4C",
+  primaryDark: "#001F4C",
+  secondary: "#283149",
+  success: "#27E6FF",
+  warning: "#D199AE",
+};
+
+export const additionalColors = {
+  binance: "#638E91",
+  overlay: "#001f4c",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
+  ...additionalColors,
+  background: "#F4F4F9",
+  backgroundDisabled: "#E8E9EB",
+  backgroundAlt: "#FFFFFF",
+  cardBorder: "#F4F4F9",
+  contrast: "#191325",
+  dropdown: "#F4F4F9",
+  dropdownDeep: "#E2E6ED",
+  invertedContrast: "#FFFFFF",
+  input: "#DFE7F2",
+  inputSecondary: "#DFE7F2",
+  tertiary: "#DEFFFE",
+  text: "#19213A",
+  textDisabled: "#BDC2C4",
+  textSubtle: "#6A93AD",
+  disabled: "#E9EAEB",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #C1D0F4 0%, #DFE7F2 100%)",
+    inverseBubblegum: "linear-gradient(139.73deg, #DFE7F2 0%, #C1D0F4 100%)",
+    cardHeader: "linear-gradient(111.68deg, #F4F4F9 0%, #E2E6ED 100%)",
+    blue: "linear-gradient(180deg, #CAEFED 0%, #C3D1E2 100%)",
+    violet: "linear-gradient(180deg, #CAEFED 0%, #C3D1E2 100%)",
+    violetAlt: "linear-gradient(180deg, #CAEFED 0%, #C3D1E2 100%)",
+    gold: "linear-gradient(180deg, #71FFFF 0%, #F2ACA2 100%)",
+  },
+};
+
+export const darkColors: Colors = {
+  ...baseColors,
+  ...additionalColors,
+  secondary: "#9A6AFF",
+  background: "#08060B",
+  backgroundDisabled: "#3c3742",
+  backgroundAlt: "#08060B",
+  cardBorder: "#31383f",
+  contrast: "#FFFFFF",
+  dropdown: "#1B1E1E",
+  dropdownDeep: "#0E1214",
+  invertedContrast: "#191326",
+  input: "#3B3F56",
+  inputSecondary: "#222B51",
+  primaryDark: "#9AEBED",
+  tertiary: "#353547",
+  text: "#E1E7F9",
+  textDisabled: "#616470",
+  textSubtle: "#C5CAD3",
+  disabled: "#4A5160",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #31475B 0%, #272B35 100%)",
+    inverseBubblegum: "linear-gradient(139.73deg, #272B35 0%, #31475B 100%)",
+    cardHeader: "linear-gradient(166.77deg, #31475B 0%, #3A3045 100%)",
+    blue: "linear-gradient(180deg, #31475B 0%, #4A5160 100%)",
+    violet: "linear-gradient(180deg, #31475B 0%, #31475B 100%)",
+    violetAlt: "linear-gradient(180deg, #31475B 0%, #31475B 100%)",
+    gold: "linear-gradient(180deg, #71FFFF 0%, #F2ACA2 100%)",
+  },
+};
